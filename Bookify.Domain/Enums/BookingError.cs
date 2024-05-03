0cee2e63e1d11feb8b24b905dce4cdf1e686ae62
mhdsbt <mhd.sabet63@gmail.com>
2024-05-03 13:10:34 +0330

    REFACTOR:Fixed Error Contact

--- Bookify.Domain/Enums/BookingError.cs
+++ Bookify.Domain/Enums/BookingError.cs
@@ -10,10 +10,10 @@ namespace Bookify.Domain.Enums
 public static class BookingErrors
     {
         public static Error NotFound = new("Booking.Found", "The Booking With the Specefied Identifier was not found!");
-        public static Error NotPending = new("normalError", "Not Pending");
-        public static Error Confirmed = new("Confirmed", "The Boooking Already is Confirm And Can not be Cancelled");
-        public static Error NotConfirm = new("NotConfirm", "The Booking Already NotConfirm");
-        public static Error Alreadystarted = new("Alreadystarted", "The Booking Already Started");
+        public static Error NotPending = new("Booking.NotPending", "Not Pending");
+        public static Error Confirmed = new("Booking.Confirmed", "The Boooking Already is Confirm And Can not be Cancelled");
+        public static Error NotConfirm = new("Booking.NotConfirm", "The Booking Already NotConfirm");
+        public static Error Alreadystarted = new("Booking.Alreadystarted", "The Booking Already Started");
 
     }
 }
