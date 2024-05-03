345b06e7b701f3462a485baf8d87eb8fceb54f58
mhdsbt <mhd.sabet63@gmail.com>
2024-05-03 13:43:29 +0330

    FEATURE:Add Application Layer and MediatR Package Also Reference Application To Domain Layer.

---
+++ Bookify.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Entities.Booking;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection Addapplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
+            });
+
+            services.AddTransient<PricingService>();
+
+            return services;
+        }
+    }
+}
--- Bookify.Domain/Enums/BookingError.cs
+++ Bookify.Domain/Enums/BookingError.cs
@@ -11,7 +11,7 @@ public static class BookingErrors
     {
         public static Error NotFound = new("Booking.Found", "The Booking With the Specefied Identifier was not found!");
         public static Error NotPending = new("Booking.NotPending", "Not Pending");
-        public static Error Confirmed = new("Booking.Confirmed", "The Boooking Already is Confirm And Can not be Cancelled");
+        public static Error Confirmed = new("Booking.Confirmed", "The Boooking Already is Confirm");
         public static Error NotConfirm = new("Booking.NotConfirm", "The Booking Already NotConfirm");
         public static Error Alreadystarted = new("Booking.Alreadystarted", "The Booking Already Started");
 
