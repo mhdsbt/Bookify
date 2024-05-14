9109ed3c0bb27a1425cda207e8ca48b995cdb57e
mehdi <mhd.sabet63@gmail.com>
2024-05-14 15:02:55 +0330

    FEATURE:Add Ef Core To the Project

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContext options) : base()
+        {
+            
+
+        }
+
+        public Task<int> SavechangeAsync(CancellationToken cancellationToken = default)
+        {
+           
+            throw new NotImplementedException();
+        }
+    }
+}
