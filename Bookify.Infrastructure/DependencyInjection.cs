c0df9cecbdfcf842a28af39ce0df3db084666f06
mehdi <mhd.sabet63@gmail.com>
2024-05-14 08:54:33 +0330

    FEATURE:Add Infrastructure Layer Project And AMil Service init

---
+++ Bookify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Bookify.Application.Abstraction.Email;
+using Bookify.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Bookify.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            services.AddTransient<IEmailService, EmailService.EmailService>();
+            return services;
+        }
+
+    }
+}
---
+++ Bookify.Infrastructure/EmailService/EmailService.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Application.Abstraction.Email;
+using Bookify.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.EmailService
+{
+    internal sealed class EmailService : IEmailService
+    {
+        public Task SendAsync(Email recipient, string subject, string body)
+        {
+            //do work here
+            return Task.CompletedTask;
+        }
+    }
+}
