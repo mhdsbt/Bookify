2933d9e50140dbd8afa880bb531bd358634f972b
mehdi <mhd.sabet63@gmail.com>
2024-05-17 11:35:39 +0330

    FEATURE:Add UserAprtment Domain Configuration

---
+++ Bookify.Infrastructure/Configuration/ApartmentConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Entities.Apratment;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Configuration
+{
+    internal sealed class ApartmentConfiguration : IEntityTypeConfiguration<Apartment>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Apartment> builder)
+        {
+            builder.ToTable("apartments");
+
+            builder.HasKey(x => x.Id);
+
+            builder.OwnsOne(apartment => apartment.Address);
+
+            builder.Property(apartment => apartment.Name).HasMaxLength(200);
+        }
+    }
+}
---
+++ Bookify.Infrastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Bookify.Domain.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Infrastructure.Configuration
+{
+    internal sealed class UserConfiguration : IEntityTypeConfiguration<Users>
+    {
+        public void Configure(EntityTypeBuilder<Users> builder)
+        {
+            builder.ToTable("users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(user => user.FirstName)
+            .HasMaxLength(128);
+
+            builder.Property(user => user.LastName)
+            .HasMaxLength(128);
+
+            builder.Property(user => user.Email)
+                .HasMaxLength(200);
+
+            
+            builder.HasIndex(u=>u.Email).IsUnique();
+                
+        }
+    }
+}
